07b345b197eb12e0138998ebd3f8077e988a59d2
Santiago Andres Gomez Rojas <santgom07@gmail.com>
2025-12-16 01:15:25 -0500

    Delete 5Rojas directory

--- 5Rojas/ConsultasSeguridad.sql
+++
@@ -1,4 +0,0 @@
-SELECT * FROM Empleado WHERE idEmpleado = 9000;
-SELECT * FROM Venta    WHERE idVenta    = 9001;
-SELECT * FROM Compra   WHERE idCompra   = 9002;
-SELECT * FROM Cliente  WHERE idCliente  = 900;
